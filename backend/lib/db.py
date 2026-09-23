afab81c517cd8cc1189e3539f87722e07f127af6
Aidan Suhendy <aidansuhendy6@gmail.com>
2026-09-23 23:23:43 +0700

    Add tls=true and tlsAllowInvalidCertificates=true to MongoDB URL

--- backend/lib/db.py
+++ backend/lib/db.py
@@ -11,7 +11,7 @@ import certifi
 
 load_dotenv(Path(__file__).parent.parent / ".env")
 
-DEFAULT_ATLAS_URL = "mongodb+srv://aidansuhendy6_db_user:Aidan12345@cluster0.wc1r21g.mongodb.net/tagihan_db?retryWrites=true&w=majority"
+DEFAULT_ATLAS_URL = "mongodb+srv://aidansuhendy6_db_user:Aidan12345@cluster0.wc1r21g.mongodb.net/tagihan_db?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true"
 
 mongo_url = os.getenv("MONGO_URL") or DEFAULT_ATLAS_URL
 db_name = os.getenv("DB_NAME", "tagihan_db")
